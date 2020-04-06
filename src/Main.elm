e2fe0c7cf4a2b1a2570268419f1108fb8a781399
Charlie Bevis <charliebevis@gmail.com>
2020-04-06 09:45:32 -0500

    simple mouse click handling

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,29 @@
 module Main exposing (..)
 
 import Browser
-import Element exposing (centerY, fill, padding, rgb, spacing, width)
+import Element exposing (centerY, el, fill, height, moveRight, padding, rgb, spacing, text, width)
 import Element.Background exposing (color)
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Element.Events exposing (onMouseDown, onMouseMove, onMouseUp)
+import Html exposing (Html)
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { isMouseDown : Bool
+    , cardPosition : Float
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { isMouseDown = False
+      , cardPosition = 0
+      }
+    , Cmd.none
+    )
 
 
 
@@ -25,11 +32,25 @@ init =
 
 type Msg
     = NoOp
+    | MouseUp
+    | MouseDown
+    | MouseMove
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        MouseDown ->
+            ( { model | isMouseDown = True }, Cmd.none )
+
+        MouseUp ->
+            ( { model | isMouseDown = False }, Cmd.none )
+
+        MouseMove ->
+            ( { model | isMouseDown = False }, Cmd.none )
 
 
 
@@ -39,9 +60,25 @@ update msg model =
 view : Model -> Html Msg
 view model =
     Element.layout [] <|
-        Element.el
-            [width fill, centerY, color (rgb 0.8 0.4 0.4), padding 30]
-         (Element.text "hello from elm-ui")
+        Element.column [ width fill, height fill ]
+            [ el (cardStyles model.isMouseDown model.cardPosition) (text "click me")
+            , Element.el
+                [ width fill, centerY, color (rgb 0.8 0.4 0.4), padding 30 ]
+                (Element.text "hello from elm-ui")
+            ]
+
+
+cardStyles isMouseDown x =
+    [ padding 100, moveRight x ]
+        ++ (if isMouseDown then
+                [ color (rgb 0.8 0.8 0.4)
+                , onMouseUp MouseUp
+                , onMouseMove MouseMove -- probably need a custom mouseMove to listen for x,y changes
+                ]
+
+            else
+                [ color (rgb 0.8 0.4 0.8), onMouseDown MouseDown ]
+           )
 
 
 
